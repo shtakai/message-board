2bf2bc0d7efdd25c41dc747cdee373cdc40a4c5e
Alyson T <shtakai@gmail.com>
2016-11-02 05:53:02 +0000

    created message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    
+    # grab all messages
+    @messages = Message.all
   end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path, notice: 'sent the message'
+  end
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  
 end
