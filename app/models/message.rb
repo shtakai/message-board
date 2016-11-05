fbb08fa14e348a03616d846009f451a24335248b
Alyson T <shtakai@gmail.com>
2016-11-05 09:15:20 +0000

    add validation on model

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,12 @@ class Message < ActiveRecord::Base
   # and body is also mandatory, 2-30 chars
   validates :body, length: {minimum: 2, maximum: 30}, presence: true
   
+  # age is mandatory, greater than equal to 0
+  validates :age,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0
+    },
+    presence: :true
+  
 end
